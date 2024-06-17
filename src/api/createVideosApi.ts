f26114b263ec4c3a4645beff4da91252f16270cf
Anel Danza <aneldanza23@gmail.com>
2024-06-17 14:06:58 -0400

    clean up

--- src/api/createVideosApi.ts
+++ src/api/createVideosApi.ts
@@ -1,34 +1,29 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import type { FetchBaseQueryMeta, FetchBaseQueryError } from "@reduxjs/toolkit/query";
+import { Video } from "../types";
 
 const baseUrl =
   "https://take-home-assessment-423502.uc.r.appspot.com/api/videos";
 
-interface videoParams {
-  userId: string;
-  description: string;
-  videoUrl: string;
-}
+
 
 export const videosApi = createApi({
-  reducerPath: "videos",
+  reducerPath: "videosApi",
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    // uploadVideo: builder.query<any, videoParams>({
-    //   query: (obj: videoParams) => {
-    //     return {
-    //       url: baseUrl,
-    //       params: obj,
-    //     };
-    //   },
-    // }),
-
-    getVideoByUserId: builder.query<videoParams, string>({
+    getVideoByUserId: builder.query<Video, string>({
       query: (userId) => {
         return {
             url: '/',
             params: {"user_id": userId}
         }
+      },
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      transformResponse: (res: any) => res.data,
+      transformErrorResponse: (val: FetchBaseQueryError, meta: FetchBaseQueryMeta | undefined, arg: string ) => {
+        console.log(val)
+        console.log(meta)
+        console.log(arg)
       }
     }),
   }),
