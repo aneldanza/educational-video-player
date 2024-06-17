aed3b1352be23fbb673d682688f1cb24e8de2961
Anel Danza <aneldanza23@gmail.com>
2024-06-17 14:46:19 -0400

    update App file to display videos

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import type { RootState } from "./app/store";
+import { useSelector } from "react-redux";
+// import { addVideos, addVideo } from "./app/videoSlice";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const videos = useSelector((state: RootState) => state.videos.videos);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex-col">
+      <nav className="flex bg-slate-200 p-2">
+        <button className="border border-black rounded-xl px-2">Upload</button>
+      </nav>
+      <main className="bg-slate-500 p-2">
+        <div>Videos</div>
+        <ul className="flex-col divide-y divide-white">
+          {videos.map((video, i) => {
+            return <li key={`video-${i}`}>{video.title}</li>;
+          })}
+        </ul>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
