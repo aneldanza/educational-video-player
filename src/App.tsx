d1db52468819ce6acd84769bd9fd22477d186f1e
Anel Danza <aneldanza23@gmail.com>
2024-06-18 13:36:31 -0400

    configure rtk fetching in the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import { useAppSelector } from "./app/hooks";
-// import { addVideos, addVideo } from "./app/videoSlice";
+import { useGetVideoByUserIdQuery } from "./api/createVideosApi";
+
+const userId = "anel_danza";
 
 function App() {
   const videos = useAppSelector((state) => state.videos.videos);
+  const { data, error, isLoading } = useGetVideoByUserIdQuery(userId);
+
+  const openModal = () => {};
 
   return (
     <div className="flex-col">
       <nav className="flex bg-slate-200 p-2 justify-between">
         <div>Logo</div>
         <div className="border border-black rounded-2xl px-5">search bar</div>
-        <button className="border border-black rounded-xl px-2 cursor-pointer">
+        <button
+          className="border border-black rounded-xl px-2 cursor-pointer"
+          onClick={openModal}
+        >
           Upload
         </button>
       </nav>
       <main className="bg-slate-500 p-2">
-        <div>Videos</div>
-        <ul className="flex-col divide-y divide-white">
-          {videos.map((video, i) => {
-            return <li key={`video-${i}`}>{video.title}</li>;
-          })}
-        </ul>
+        {error ? (
+          <div>there was an error!</div>
+        ) : isLoading ? (
+          <div>Loading...</div>
+        ) : data ? (
+          <ul className="flex-col divide-y divide-white">
+            {videos.map((video, i) => {
+              return <li key={`video-${i}`}>{video.title}</li>;
+            })}
+          </ul>
+        ) : null}
       </main>
     </div>
   );
--- src/api/createVideosApi.ts
+++ src/api/createVideosApi.ts
@@ -1,32 +1,55 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import type { FetchBaseQueryMeta, FetchBaseQueryError } from "@reduxjs/toolkit/query";
+import {
+  createApi,
+  fetchBaseQuery,
+  BaseQueryFn,
+  FetchArgs,
+} from "@reduxjs/toolkit/query/react";
+import type {
+  FetchBaseQueryMeta,
+  FetchBaseQueryError,
+} from "@reduxjs/toolkit/query";
 import { Video } from "../types";
 
-const baseUrl =
-  "https://take-home-assessment-423502.uc.r.appspot.com/api";
+const baseUrl = "https://take-home-assessment-423502.uc.r.appspot.com/api";
 
+const baseQuery = fetchBaseQuery({
+  baseUrl,
+  credentials: "same-origin",
+  prepareHeaders: (headers) => headers.set("Content-Type", "application/json"),
+});
 
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  return await baseQuery(args, api, extraOptions);
+};
 
 export const videosApi = createApi({
   reducerPath: "videosApi",
-  baseQuery: fetchBaseQuery({ baseUrl }),
+  baseQuery: await baseQueryWithReauth,
   endpoints: (builder) => ({
     getVideoByUserId: builder.query<Video, string>({
       query: (userId) => {
         return {
-            url: '/videos',
-            params: {"user_id": userId}
-        }
+          url: "/videos",
+          params: { user_id: userId },
+        };
       },
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       transformResponse: (res: any) => res.data,
-      transformErrorResponse: (val: FetchBaseQueryError, meta: FetchBaseQueryMeta | undefined, arg: string ) => {
-        console.log(val)
-        console.log(meta)
-        console.log(arg)
-      }
+      transformErrorResponse: (
+        val: FetchBaseQueryError,
+        meta: FetchBaseQueryMeta | undefined,
+        arg: string
+      ) => {
+        console.log(val);
+        console.log(meta);
+        console.log(arg);
+      },
     }),
   }),
 });
 
-export const { useGetVideoByUserIdQuery } = videosApi
+export const { useGetVideoByUserIdQuery } = videosApi;
